5a86402b214277d8ddb2b72fe77cbbc751f69536
f1nbar <finbarday@gmail.com>
2020-04-15 16:40:14 +0100

    added start of gaddag

--- Gooses.java
+++ Gooses.java
@@ -1,173 +1,176 @@
 import java.util.Collection;
 import java.util.TreeMap;
 
+import javax.sound.sampled.ReverbType;
+
+import Trie_starter.TrieNode;
 
 public class Gooses implements BotAPI {
 
-    // The public API of Bot must not change
-    // This is ONLY class that you can edit in the program
-    // Rename Bot to the name of your team. Use camel case.
-    // Bot may not alter the state of the game objects
-    // It may only inspect the state of the board and the player objects
+	// The public API of Bot must not change
+	// This is ONLY class that you can edit in the program
+	// Rename Bot to the name of your team. Use camel case.
+	// Bot may not alter the state of the game objects
+	// It may only inspect the state of the board and the player objects
 
-    private PlayerAPI me;
-    private OpponentAPI opponent;
-    private BoardAPI board;
-    private UserInterfaceAPI info;
-    private DictionaryAPI dictionary;
-    private int turnCount;
+	private PlayerAPI me;
+	private OpponentAPI opponent;
+	private BoardAPI board;
+	private UserInterfaceAPI info;
+	private DictionaryAPI dictionary;
+	private int turnCount;
 
-    Gooses(PlayerAPI me, OpponentAPI opponent, BoardAPI board, UserInterfaceAPI ui, DictionaryAPI dictionary) {
-        this.me = me;
-        this.opponent = opponent;
-        this.board = board;
-        this.info = ui;
-        this.dictionary = dictionary;
-        turnCount = 0;
-    }
+	Gooses(PlayerAPI me, OpponentAPI opponent, BoardAPI board, UserInterfaceAPI ui, DictionaryAPI dictionary) {
+		this.me = me;
+		this.opponent = opponent;
+		this.board = board;
+		this.info = ui;
+		this.dictionary = dictionary;
+		turnCount = 0;
+	}
 
-    // nested node class for GADDAG data structure
-    private static class Node implements Comparable<Node>{
-        private boolean finite;
-        private char value;
-        public TreeMap<Character, Node> tree = new TreeMap<>();
+	// nested node class for GADDAG data structure
+	private static class Node implements Comparable<Node> {
+		private boolean finite;
+		private char value;
+		public TreeMap<Character, Node> tree = new TreeMap<>();
 
-        public Node(char value) {
-            this.value = value;
-            this.finite = false;
-        }
+		public Node(char value) {
+			this.value = value;
+			this.finite = false;
+		}
 
-        // getters and setters
-        public char getValue() {
-            return this.value;
-        }
-        public void setFinite(boolean finite) {
-            this.finite = finite;
-        }
-        public boolean getFinite() {
-            return finite;
-        }
-        public Collection<Character> getKeySet() {
-            return tree.keySet();
-        }
-        public Collection<Node> getChildrenValues(){
-            return tree.values();
-        }
-        public Node getChild(char value) {
-            return tree.get(value);
-        }
+		// getters and setters
+		public char getValue() {
+			return this.value;
+		}
 
-        // info methods
-        public boolean hasChild(char value) {
-            return tree.containsKey(value);
-        }
+		public void setFinite(boolean finite) {
+			this.finite = finite;
+		}
 
-        // modifier methods
-        public void addChild(char value) {
-            tree.put(value, new Node(value));
-        }
+		public boolean getFinite() {
+			return finite;
+		}
 
-        @Override
-        public boolean equals(Object obj) {
-           if(obj instanceof Node){
-               return (this.value == ((Node) obj).getValue());
-           } else {
-               return false;
-           }
-        }
+		public Collection<Character> getKeySet() {
+			return tree.keySet();
+		}
 
-        @Override
-        public int compareTo(Node node) {
-            return this.value - node.value;
-        }
-    }
-    /* END OF NESTED NODE CLASS */
-    
-    private static class Trie {
-    	
-    	static final int ALPHABET_SIZE = 26; 
-    	
-    	static class TrieNode { 
-    		
-    		TrieNode[] children = new TrieNode[ALPHABET_SIZE]; 
-    		// isEndOfWord is true if the node represents end of a word i.e. leaf node
-    		boolean isEndOfWord; 
+		public Collection<Node> getChildrenValues() {
+			return tree.values();
+		}
 
-    		TrieNode(){ 
-    		isEndOfWord = false; 
+		public Node getChild(char value) {
+			return tree.get(value);
+		}
 
-    		for (int i = 0; i < ALPHABET_SIZE; i++) 
-    		children[i] = null; 
-    		} 
-    		}
+		// info methods
+		public boolean hasChild(char value) {
+			return tree.containsKey(value);
+		}
 
-    		static TrieNode root;
-    		// If not key present, inserts into trie 
-    		// If the key is prefix of Trie node,  
-    		//  marks leaf node
-    		static void insert(String key){ 
-    			
-    			int length;
-    			int index;
-    			
-    			TrieNode trie = root; 
-    			
-    			length = key.length();
-    			
-    			  for (int level = 0; level < length; level++) 
-    		      { 
-    		          index = key.charAt(level) - 'a'; 
-    		          if (trie.children[index] == null) 
-    		              trie.children[index] = new TrieNode(); 
-    		     
-    		          trie = trie.children[index]; 
-    		      } 
-    			
-    			trie.isEndOfWord = true;
-    		} 
+		// modifier methods
+		public void addChild(char value) {
+			tree.put(value, new Node(value));
+		}
 
-    		// Returns true if key presents in trie, else false 
-    		static boolean search(String key) { 
-    			int length;
-    			int index;
-    			
-    			TrieNode trie = root; 
-    			
-    			length = key.length();
-    			
-    			for(int level = 0; level < length; level++) {
-    				index = key.charAt(level) - 'a';
-    				
-    				if(trie.children[index] == null) {
-    					return false;		
-    				}
-    			
-    				trie = trie.children[index];
-    			} 
-    			return(trie != null && trie.isEndOfWord);
-    			
-    			
-    		} 
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof Node) {
+				return (this.value == ((Node) obj).getValue());
+			} else {
+				return false;
+			}
+		}
 
-    	
-    }
-    /* END OF NESTED TRIE CLASS */
+		@Override
+		public int compareTo(Node node) {
+			return this.value - node.value;
+		}
+	}
+	/* END OF NESTED NODE CLASS */
 
-    public String getCommand() {
-        // Add your code here to input your commands
-        // Your code must give the command NAME <botname> at the start of the game
-        String command = "";
-        switch (turnCount) {
-            case 0:
-                command = "NAME gooses";
-                break;
-            case 1:
-                //TODO make move
-        }
-        turnCount++;
-        return command;
-    }
+	// Nested Class for Gaddag Implementation
+
+	private static class Gaddag {
+
+		static final int ALPHABET_SIZE = 26;
+		private static char seperator = '>';
+		Node root;
+
+		public Gaddag() {
+			root = new Node(' ');
+		}
+		
+		static void add(String key){ 
+			
+			int length;
+			int index;
+			
+			Node trie = root; 
+			
+			length = key.length();
+			
+			  for (int level = 0; level < length; level++) 
+		      { 
+		          index = key.charAt(level) - 'a'; 
+		          if (trie.children[index] == null) 
+		              trie.children[index] = new Node(); 
+		     
+		          trie = trie.children[index]; 
+		      } 
+			
+			trie.isEndOfWord = true;
+		} 
+
+
+		static void insert(String key) {
+
+			key = key.toLowerCase();
+			int keyLength = key.length();
+
+			if (keyLength == 0) {
+				return;
+			}
+
+			String prefix;
+			char[] chars;
+			int i;
+			for (i = 1; i < keyLength; i++) {
+				prefix = key.substring(0, i);
+				chars = prefix.toCharArray();
+				reverse(chars);
+				add(new String(chars) + seperator + key.substring(i));
+			}
+			chars = key.toCharArray();
+			reverse(chars);
+			add(new String(chars) + seperator + key.substring(i));
+		}
+
+		private static void reverse(char[] toReverse) {
+			for (int i = 0; i < toReverse.length / 2; i++) {
+				int temp = toReverse[i];
+				toReverse[i] = toReverse[toReverse.length - i - 1];
+				toReverse[toReverse.length - i - 1] = (char) temp;
+			}
+		}
+	}
+	/* END OF NESTED Gaddag CLASS */
+
+	public String getCommand() {
+		// Add your code here to input your commands
+		// Your code must give the command NAME <botname> at the start of the game
+		String command = "";
+		switch (turnCount) {
+		case 0:
+			command = "NAME gooses";
+			break;
+		case 1:
+			// TODO make move
+		}
+		turnCount++;
+		return command;
+	}
 
 }
-
-
